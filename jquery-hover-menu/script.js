12d00cf580d173da3a458a9fe61e50d3e468f318
yelim03 <yelim5570@gmail.com>
2024-01-10 23:43:42 +0900

    jquery

---
+++ jquery-hover-menu/script.js
@@ -0,0 +1,7 @@
+$(".navi li").mouseenter(function() {
+  let changeImage = $(this).attr("data-image");
+  $(".photo").css({background: `url(${changeImage})`});
+});
+$(".navi li").mouseleave(function() {
+  $(".photo").css({ background: ""});
+});
